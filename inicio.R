445628c6bde810a95055e0c78b5b351b3ec3165b
Angel Moreno <42397892+angisabel44@users.noreply.github.com>
2018-09-04 01:58:04 -0500

    Add files via upload

---
+++ inicio.R
@@ -0,0 +1,19 @@
+inicio <- function() {
+  direccion <- sample(1:4, 1)
+  xg <- NULL
+  yg <- NULL
+  if (direccion == 1) { # vertical
+    xg <- 1
+    yg <- sample(1:n, 1)
+  } else if (direccion == 2) { # horiz izr -> der
+    xg <- sample(1:n, 1)
+    yg <- 1
+  } else if (direccion == 3) { # horiz der -> izq
+    xg <- n
+    yg <- sample(1:n, 1)
+  } else { # vertical al reves
+    xg <- sample(1:n, 1)
+    yg <- n
+  }
+  return(c(xg, yg))
+}
